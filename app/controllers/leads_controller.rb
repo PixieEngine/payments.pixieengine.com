2440c2d2b02c7a1afb0467b45e37546456f6cd65
Daniel Moore <yahivin@gmail.com>
2012-11-22 18:42:08 -0800

    Respond status ok

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -2,6 +2,6 @@ class LeadsController < ApplicationController
   def create
     lead = Lead.create params[:lead]
 
-    render :json => lead, :callback => params[:callback]
+    render :json => {:status => :ok}, :callback => params[:callback]
   end
 end
