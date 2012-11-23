8be12f29407074a124b44accdca58359f63b7600
Daniel Moore <yahivin@gmail.com>
2012-11-22 18:00:50 -0800

    Creating via get

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Payments::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  get "leads" => "leads#create"
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -11,7 +13,7 @@ Payments::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  resources :leads
+  # resources :products
 
   # Sample resource route with options:
   #   resources :products do
