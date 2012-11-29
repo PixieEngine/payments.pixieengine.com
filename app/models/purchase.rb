df3a0bf99d940632bba1ae1da12949879304d19c
Daniel Moore <yahivin@gmail.com>
2012-11-29 09:33:31 -0800

    Added payments

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,16 @@
 class Purchase < ActiveRecord::Base
   attr_accessible :email, :name, :product, :token
+
+  after_create :make_payment
+
+  def make_payment
+    price = 1500
+
+    Stripe::Charge.create(
+      :amount => price,
+      :currency => "usd",
+      :card => token,
+      :description => "#{email} :: #{product}"
+    )
+  end
 end
---
+++ config/initializers/stripe.rb
@@ -0,0 +1,2 @@
+Stripe.api_key = ENV["STRIPE_SECRET_KEY"] || "sk_test_cQrLX3cSHhoglpQLnpiAfJEm"
+STRIPE_PUBLIC_KEY = "pk_test_VzaTm4NaT9nrZZ94WM8tqmDb"
