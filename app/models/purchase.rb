28fd9c98de865bce1994008b647ed5eb9f4943cf
Daniel Moore <yahivin@gmail.com>
2012-11-29 20:38:35 -0800

    Added sending mail to purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -12,5 +12,9 @@ class Purchase < ActiveRecord::Base
       :card => token,
       :description => "#{email} :: #{product}"
     )
+
+    person = Person.new(:name => name, :email => email)
+    Notifier.purchase_notification(person, product).deliver
+
   end
 end
