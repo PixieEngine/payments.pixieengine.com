a78ff43091832d848f8dd03a89101db4793e449f
Daniel Moore <yahivin@gmail.com>
2012-11-29 20:40:04 -0800

    Revert "Trying more lenient cors"
    This reverts commit e1935d948667978343d6d16f936cfaf34538ff33.

---
+++ config/initializers/cors.rb
@@ -0,0 +1,8 @@
+Payments::Application.config.middleware.use Rack::Cors do
+  allow do
+    origins 'redicegame.com'
+    resource "*",
+      :headers => ['Origin', 'Accept', 'Content-Type'],
+      :methods => [:get, :post, :options]
+  end
+end
